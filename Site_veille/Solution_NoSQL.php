b8a96d11d029f8dd9e6f7f91b595535094ec2406
Nialee37 <43949445+Nialee37@users.noreply.github.com>
2018-12-10 08:43:58 +0100

    Add files via upload

---
+++ Site_veille/Solution_NoSQL.php
@@ -0,0 +1,103 @@
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Veille NoSQL</title>
+		<link rel="stylesheet" type="text/css" href="style_Index.css">
+	</head>
+	<header>
+		<p class="Titre"><img src="Images/nosql.png" alt""/></p>
+	</header>
+
+	<body>
+		<div class="textList">
+			<a>
+				<ul id="ListRedis">				
+					<li>
+						<h4>
+							</br>&nbsp REDIS
+						</h4>
+						Redis est un système de gestion de base de données clef-valeur scalable.</br>Le principe de redis est de stocker les données utiliser en RAM, ce qui permet d'obtenir d'exellentes performances en évitant les accès disques.</br>La memoires RAM étant volatile, il est possible de capturer l'état de la base dans un fichier, chaque capture sert autant de sauvegarde, que de backup en cas d'incident. Les modifications faite entre les captures ne sont donc pas visible.</br></br>Ce système permet de manipuler des types de données simples (chaînes de caractère, tableaux associatifs, listes ensembles et ensembles ordonnés).
+					</li>
+				</ul>
+			</a>
+		</div>
+		<div class="textList">
+			<a>
+				<ul id="ListCassandra">
+					<li>
+						<h4>
+							</br>&nbsp CASSANDRA
+						</h4>
+						Cassandra est un système de gestion de base de données conçu pour gérer de grosse quantités de données sur un grand nombre de serveurs. Ce qui permet une répartition robuste sur plusieurs centres de données avec une réplication asynchrone et une faible latence pour les opérations de tous les clients.</br>initialement développée par Facebook, l'application a été libérée dans l'espace open source en juillet 2008, et rapidement adoptée par des entreprises gérant d'importants volumes de données, telles que Twitter, Netflix ou Spotify.
+					</li>
+				</ul>
+			</a>
+		</div>
+		<div class="textList">
+			<a>
+				<ul id="ListMongodb">
+					<li>
+						<h4>
+							</br>&nbsp MONGODB
+						</h4>
+						MongoDB est un système de gestion de base de données orientée documents, répartissable sur un nombre quelconque d'ordinateurs et ne nécessitant pas de schéma prédéfini des données. Il est écrit en C++. Le serveur et les outils sont distribués sous licence AGPL, les pilotes sous licence Apache et la documentation sous licence Creative Commons.</br>MongoDB permet de manipuler des objets structurés au format BSON, sans schéma prédéterminé. En d'autres termes, des clés peuvent être ajoutées à tout moment, sans reconfiguration de la base.</br>Contrairement aux bases de données relationnelles, les champs d'un enregistrement sont libres et peuvent être différents d'un enregistrement à un autre au sein d'une même collection. Le seul champ commun et obligatoire est le champ de clé principale. Par ailleurs, MongoDB ne permet ni les requêtes très complexes standardisées, ni les JOIN, mais permet de programmer des requêtes spécifiques en JavaScript.
+					</li>
+				</ul>
+			</a>
+		</div>
+		<div class="textList">
+			<a>
+				<ul id="ListNeo4j">
+					<li>
+						<h4>
+							</br>&nbsp NEO4J
+						</h4>
+						Neo4j est un système de gestion de base de données au code source libre basée sur les graphes, développé en Java1,2 par la société suédo-américaine Neo technology.</br>Neo4j permet de représenter les données en tant que nœuds reliés par un ensemble d'arcs, ces objets possédant leurs propres propriétés. Les propriétés sont constitués d'un couple de clé-valeurs de type simple tel que chaînes de caractères ou numérique; celles-ci peuvent êtres indexées. La modélisation est très proche des concepts métier, il n'est pas nécessaire d'utiliser de clés dans Neo4j, car les relations ont une existence propre. L'absence de modélisation rigide rend Neo4j bien adapté à la gestion de données changeantes et de schémas évoluant fréquemment.
+					</li>
+				</ul>
+			</a>
+		</div>
+		<div class="textList">
+			<a>
+				<ul id="oui">
+					<li>
+						</br>
+						</br>
+						</br>
+						</br>
+						</br>
+						</br>
+						</br>
+						</br>
+						</br>
+						</br>
+						</br>
+						</br>
+
+					</li>
+				</ul>
+			</a>
+		</div>
+
+	</body>
+	<footer>
+		<div id="particles-js2"></div>
+		<script type="text/javascript" src="particles.js"></script>
+		<script type="text/javascript" src="app.js"></script>
+	</footer>
+	<sidebar>
+	  	<div>
+	     	<nav>
+	        	<ul id="menuSo">
+	        	 	<li><a id="menuSo1" href="index.html" >Accueil</li>
+	        	 		<br>
+	            	<li><a id="menuSo2" href="NoSQL.html" >NoSQL c'est quoi ?</li>
+	            		<br>
+	            	<li><a id="menuSo2" href="Solution_NoSQL.php" >&nbsp&nbsp Solutions NoSQL</li>
+	            		<br>
+	            	<li><a id="menuNo2" href="Telecharger.html" >Ou les telecharger ?</li>
+				</ul>
+	    	</nav> 
+		</div>
+	</sidebar>
+</html>
